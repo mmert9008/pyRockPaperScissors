d4950ec1af4aca14f7da5adc68aa484f01475bab
mmert9008 <michael.mertens.jr@gmail.com>
2025-06-14 00:09:09 -0500

    implemented main function

--- main.py
+++ main.py
@@ -1,43 +1,50 @@
 import random
 
-player_wins = 0
-computer_wins = 0
 
-opts = ["rock", "paper", "scissors"]
+def main():
+    player_wins = 0
+    computer_wins = 0
 
-while True:
-    turn = input("Choose Rock, Paper, or Scissors or press Q to quit: ").lower()
-    if turn == "q":
-        break
+    opts = ["rock", "paper", "scissors"]
 
-    if turn not in opts:
-        continue
-    
-    rng = random.randint(0, 2)
-    # rock = 0, paper = 1, scissors = 2
+    while True:
+        turn = input("Choose Rock, Paper, or Scissors or press Q to quit: ").lower()
+        if turn == "q":
+            break
 
-    computer_turn = opts[rng]
-    print(f"Computer threw {computer_turn}")
+        if turn not in opts:
+            continue
 
-    if turn == "rock" and computer_turn == "scissors":
-        print("You win!\n")
-        player_wins += 1
-    elif turn == "paper" and computer_turn == "rock":
-        print("You win!\n")
-        player_wins += 1
-    elif turn == "scissors" and computer_turn == "paper":
-        print("You win!\n")
-        player_wins += 1
-    elif turn == computer_turn:
-        print("Go again, no points!\n")
+        rng = random.randint(0, 2)
+        # rock = 0, paper = 1, scissors = 2
+
+        computer_turn = opts[rng]
+        print(f"Computer threw {computer_turn}")
+
+        if turn == "rock" and computer_turn == "scissors":
+            print("You win!\n")
+            player_wins += 1
+        elif turn == "paper" and computer_turn == "rock":
+            print("You win!\n")
+            player_wins += 1
+        elif turn == "scissors" and computer_turn == "paper":
+            print("You win!\n")
+            player_wins += 1
+        elif turn == computer_turn:
+            print("Go again, no points!\n")
+        else:
+            print("You lose, git gud!\n")
+            computer_wins += 1
+
+    print(f"\nYou won {player_wins} times.")
+    print(f"The computer won {computer_wins} times.")
+    if player_wins > computer_wins:
+        print("You are the Rock Paper Scissors MASTER!")
     else:
-        print("You lose, git gud!\n")
-        computer_wins += 1
+        print("Git gud, sucka!")
+    print("Game over!")
 
-print(f"\nYou won {player_wins} times.")
-print(f"The computer won {computer_wins} times.")
-if player_wins > computer_wins:
-    print("You are the Rock Paper Scissors MASTER!")
-else:
-    print("Git gud, sucka!")
-print("Game over!")
+
+if __name__ == "__main__":
+    main()
+    main()
